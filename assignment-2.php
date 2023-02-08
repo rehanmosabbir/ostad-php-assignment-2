35fc620e258fe9f62a7a43f0ba679ac706dd9484
rehanmosabbir <rehanmosabbir@gmail.com>
2023-02-08 20:14:49 +0600

    modify assignment-2.php

--- assignment-2.php
+++ assignment-2.php
@@ -4,4 +4,4 @@ $tuitionFee = 20000;
 
 $commission = ( $tuitionFee >= 20000 ) ? $tuitionFee * 0.25 : (  ( $tuitionFee >= 10000 && $tuitionFee < 20000 ) ? $tuitionFee * 0.2 : (  ( $tuitionFee >= 7000 && $tuitionFee < 10000 ) ? $tuitionFee * 0.15 : (  ( $tuitionFee < 7000 ) ? "The data is Invalid" : null ) ) );
 
-echo $commission;
+echo "Commission = {$commission}";
